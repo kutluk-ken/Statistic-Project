abd40094e1a7ea2fab2afc623db89778aec804d0
Ken <kutlukanwar@gmail.com>
2023-06-18 13:50:35 -0400

    Basic Structure

--- Rshiny-Apopo/ui.R
+++ Rshiny-Apopo/ui.R
@@ -9,10 +9,51 @@
 
 library(shiny)
 library(readxl)
+library(shinydashboard)
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  mainPanel(
-    verbatimTextOutput("summary")
+# ui <- fluidPage(
+#   mainPanel(
+#     verbatimTextOutput("summary")
+#   )
+# )
+ui <- dashboardPage(
+  dashboardHeader(title = "My Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overview", tabName = "Overview", icon = icon("dashboard")),
+      menuItem("Rat Performance", tabName = "Rat_Performance", icon = icon("chart-bar")),
+      menuItem("Reused Analyst", tabName = "Reused_Analyst", icon = icon("recycle")),
+      menuItem("Feedback", tabName = "Feedback", icon = icon("envelope")),
+      menuItem("something1", tabName = "something1", icon = icon("question")),
+      menuItem("something2", tabName = "something2", icon = icon("recycle")),
+      menuItem("something3", tabName = "something3", icon = icon("recycle"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("Overview",
+              h2("Overview Tab Content")
+      ),
+      tabItem("Rat_Performance",
+              h2("Rat Performance Tab Content")
+      ),
+      tabItem("Reused_Analyst",
+              h2("Reused Tab Content")
+      ),
+      tabItem("Feedback",
+              h2("Feedback Tab Content")
+      ),
+      tabItem("something1",
+              h2("Reused Tab Content")
+      ),
+      tabItem("something2",
+              h2("Reused Tab Content")
+      ),
+      tabItem("something3",
+              h2("Reused Tab Content")
+      )
+    )
   )
 )
