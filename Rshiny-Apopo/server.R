bba54e8b8d631c65826c4491f765d75132ded909
Ken <kutlukanwar@gmail.com>
2023-06-18 13:10:23 -0400

    Read data, display summary.

--- Rshiny-Apopo/server.R
+++ Rshiny-Apopo/server.R
@@ -8,21 +8,17 @@
 #
 
 library(shiny)
+library(readxl)
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+function(input, output, session){
+  # Read XLSX file
+  data <- reactive({
+    read_excel("/Users/kutlukanwar/Desktop/STAD94/TBdetectionRats_Tanzania.xlsx", sheet = 2, col_names = TRUE)
+  })
+  
+  # Output summary
+  output$summary <- renderPrint({
+    data()
+  })
 }
