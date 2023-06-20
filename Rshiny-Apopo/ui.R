5f08b95c0c646d7c27ea499f916f325d979edb78
HuairuChen <95056482+HuairuChen@users.noreply.github.com>
2023-06-19 23:29:44 -0400

    Update ui.R
    Update UI corresponding to server

--- Rshiny-Apopo/ui.R
+++ Rshiny-Apopo/ui.R
@@ -10,8 +10,6 @@
 library(shiny)
 library(readxl)
 library(shinydashboard)
-library(ggplot2)
-
 
 
 # Define UI for application that draws a histogram
@@ -25,7 +23,9 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Overview", tabName = "Overview", icon = icon("dashboard")),
-      menuItem("Rat Performance", tabName = "Rat_Performance", icon = icon("chart-bar")),
+      menuItem("Rat Performance", tabName = "Rat_Performance", icon = icon("chart-pie")),
+      menuItem("Trainer Analysis", tabName = "Trainer_Analysis", icon = icon("chart-bar")),
+      menuItem("Time Control", tabName = "Control_Time_Analysis", icon = icon("clock")),
       menuItem("Reused Analyst", tabName = "Reused_Analyst", icon = icon("recycle")),
       menuItem("Feedback", tabName = "Feedback", icon = icon("envelope")),
       menuItem("something1", tabName = "something1", icon = icon("question")),
@@ -41,7 +41,34 @@ ui <- dashboardPage(
               )
       ),
       tabItem("Rat_Performance",
-              h2("Rat Performance Tab Content")
+              h2("Rat Performance Tab Content"),
+              tabsetPanel(
+                tabPanel("Sen", tableOutput("basicInformation_Sensitivity")),
+                tabPanel("Spe", tableOutput("basicInformation_Specificity")),
+                tabPanel("Ind per", plotOutput("Individual_rat_performance"))
+              )
+      ),
+      tabItem("Trainer_Analysis",
+              h2("Analysis Trainer by time"),
+              tabsetPanel(
+                tabPanel("O_Sen", tableOutput("Trainer_Sensitivity")),
+                tabPanel("O_Spe", tableOutput("Trainer_Specificity")),
+                tabPanel("D_Sen", tableOutput("Sensitivity_trainer_table_daily")),
+                tabPanel("D_Spe", tableOutput("Specificity_trainer_table_daily")),
+                tabPanel("W_Sen", tableOutput("Sensitivity_trainer_table_weekly")),
+                tabPanel("W_Spe", tableOutput("Specificity_trainer_table_weekly")),
+                tabPanel("M_Sen", tableOutput("Sensitivity_trainer_table_monthly")),
+                tabPanel("M_Spe", tableOutput("Specificity_trainer_table_monthly"))
+              )
+      ),
+      tabItem("Control_Time_Analysis",
+              h2("Time"),
+              uiOutput("valueSelect"),
+              uiOutput("trainerSelect"),
+              uiOutput("timeSelect"),
+              plotOutput("LineChart"),
+              tableOutput("trainertable")
+              
       ),
       tabItem("Reused_Analyst",
               h2("Reused Tab Content")
@@ -53,7 +80,7 @@ ui <- dashboardPage(
               h2("Reused Tab Content")
       ),
       tabItem("something2",
-              h2("Reused Tab Content")
+              h2("Reused Tab Content")  
       ),
       tabItem("something3",
               h2("Reused Tab Content")
